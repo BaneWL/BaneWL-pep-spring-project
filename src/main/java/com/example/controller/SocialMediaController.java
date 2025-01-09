009a628be23a1b4d6d9d1703dad2f11c144bb0f6
Bane Lausterer <banelausterer@gmail.com>
2025-01-09 08:23:32 +0000

    Progress

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -1,5 +1,9 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+import com.example.entity.Account;
+import com.example.entity.Message;
 
 /**
  * TODO: You will need to write your own endpoints and handlers for your controller using Spring. The endpoints you will need can be
@@ -7,6 +11,45 @@ package com.example.controller;
  * where applicable as well as the @ResponseBody and @PathVariable annotations. You should
  * refer to prior mini-project labs and lecture materials for guidance on how a controller may be built.
  */
+@RestController
 public class SocialMediaController {
+    @PostMapping("/register/")
+    public Account registerAccount(@RequestBody Account account){
+        return null;
+    }
 
+    @PostMapping("/login/")
+    public Account loginAccount(@RequestBody Account account){
+        return null;
+    }
+
+    @PostMapping("/messages/")
+    public Message postMessage(@RequestBody Message message){
+        return null;
+    }
+
+    @GetMapping("/messages/")
+    public Message getMessages(){
+        return null;
+    }
+
+    @GetMapping("/messages/{message_id}/")
+    public Message getMessage(@PathVariable int id){
+        return null;
+    }
+
+    @DeleteMapping("/messages/{message_id}/")
+    public Message deleteMessage(@PathVariable int id){
+        return null;
+    }
+
+    @PatchMapping("/messages/{message_id}/")
+    public Message updatMessage(@RequestBody Message message, @PathVariable int id){
+        return null;
+    }
+
+    @GetMapping("/accounts/{account_id}/messages/")
+    public Message getUserMessage(@PathVariable int id){
+        return null;
+    }
 }
